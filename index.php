c20058db23dcfe3608f71959933d0c4a63cdf2ba
André Moura <andre.benedicto@etec.sp.gov.br>
2019-10-16 19:53:44 -0300

    Tela de login

--- index.php
+++ index.php
@@ -1,10 +1,13 @@
 <?php 
 
+session_start ( );
+
 require_once("vendor/autoload.php");
 
 use \Slim\Slim;
 use \AndreMoura\Page;
 use \AndreMoura\PageAdmin;
+use \AndreMoura\Model\User;
 
 $app = new Slim();
 
@@ -20,10 +23,39 @@ $app->get('/', function() {
 
 $app->get('/admin', function() {
     
+	User::verifyLogin ( );
+
 	$page = new PageAdmin ( );
 
 	$page -> setTpl ( 'index' );
 
+});
+
+$app->get('/admin/login', function() {
+    
+	$page = new PageAdmin ( [
+		'header' => false,
+		'footer' => false
+	]);
+
+	$page -> setTpl ( 'login' );
+
+});
+
+$app -> post ( '/admin/login', function ( ) {
+	User::login ( $_POST['login'], $_POST['password'] );
+
+	header ( 'Location: /admin');
+	exit;
+
+});
+
+$app -> get ( '/admin/logout', function ( ){
+
+	User::logout ( );
+
+	header ( 'Location: /admin/login' );
+	exit;
 });
 
 $app->run();
