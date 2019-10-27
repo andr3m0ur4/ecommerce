35c6a4ae7ae8b92debc2275e28daf6651f25e8a7
André Moura <andre.benedicto@etec.sp.gov.br>
2019-10-27 12:25:31 -0300

    Admin - Products - Search and Pagination

--- admin-products.php
+++ admin-products.php
@@ -8,12 +8,39 @@ $app -> get ( '/admin/products', function ( ) {
 
 	User::verifyLogin ( );
 
-	$products = Product::listAll( );
+	$search = isset ( $_GET['search'] ) ? $_GET['search'] : '';
+
+	$page = isset ( $_GET['page'] ) ? ( int ) $_GET['page'] : 1;
+
+	if ( $search != '' ) {
+
+		$pagination = Product::getPageSearch ( $search, $page );
+
+	} else {
+
+		$pagination = Product::getPage ( $page );
+
+	}
+
+	$pages = [];
+
+	for ( $x = 1; $x <= $pagination['pages']; $x++ ) {
+
+		array_push ( $pages, [
+			'href' => '/admin/products?' . http_build_query ( [
+				'page' => $x,
+				'search' => $search
+			] ),
+			'text' => $x
+		]);
+	}
 
 	$page = new PageAdmin ( );
 
-	$page -> setTpl ( 'products', array ( 
-		'products' => $products
+	$page -> setTpl ( 'products', array (
+		'products' => $pagination['data'],
+		'search' => $search,
+		'pages' => $pages
 	) );
 
 });
